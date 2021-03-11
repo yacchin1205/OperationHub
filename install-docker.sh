5ed571ae25cb0d434b849162afe0f8a182422857
yacchin1205 <yazawa@yzwlab.net>
2021-03-11 11:13:46 +0900

    Add https-portal service

--- install-docker.sh
+++ install-docker.sh
@@ -17,7 +17,6 @@ yum install -y docker-ce docker-ce-cli containerd.io
 systemctl enable docker.service
 systemctl start docker.service
 
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-
