472416307ca2b75bb554df1eb815a4463f04a4a3
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-07-31 21:22:32 +0000

    Add example

--- config/nbsearch/jupyterhub_config.py
+++ config/nbsearch/jupyterhub_config.py
@@ -1,9 +1,15 @@
 service_name  = 'solr'
 
+proxy_external_url = 'https://xxx.xxx.xxx.xxx'
+
 c.JupyterHub.services.append(
     {
         'name': service_name,
         'admin': True,
         'url': f'http://nbsearch-{service_name}-proxy',
+        'oauth_client_id': f'service-{service_name}',
+        'api_token': '0123456789ABCDEFGHIJKLMN',
+        'oauth_redirect_uri': f'{proxy_external_url}/services/solr/oauth2/callback',
+        'oauth_no_confirm': True,
     }
 )
